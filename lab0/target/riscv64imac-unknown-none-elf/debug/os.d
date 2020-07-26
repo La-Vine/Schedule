a15d4239fa257b062de307df2ac8e9a4b1f6b41f
La-Vine <1286661681@qq.com>
2020-07-26 12:57:53 +0800

    os

---
+++ lab0/target/riscv64imac-unknown-none-elf/debug/os.d
@@ -0,0 +1,1 @@
+/home/zzy/Project/os/target/riscv64imac-unknown-none-elf/debug/os: /home/zzy/Project/os/src/console.rs /home/zzy/Project/os/src/entry.asm /home/zzy/Project/os/src/main.rs /home/zzy/Project/os/src/panic.rs /home/zzy/Project/os/src/sbi.rs
